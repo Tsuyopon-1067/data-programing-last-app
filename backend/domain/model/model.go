86c0906cce0dc624eef03f9e4cb77aea0d418f31
Shibatch <lawnbone.tubist@gmail.com>
2024-06-26 10:38:06 +0900

    ♻️ タイムスタンプの型をstringに変更

--- backend/domain/model/model.go
+++ backend/domain/model/model.go
@@ -1,15 +1,13 @@
 package model
 
-import "time"
-
 type Message struct {
 	Username string `json:"username"`
 	Message  string `json:"message"`
-	TimeStamp time.Time `json:"timestamp"`
+	TimeStamp string `json:"timestamp"`
 }
 
 type Response struct {
 	Username string `json:"username"`
 	Message  string `json:"message"`
-	TimeStamp time.Time `json:"timestamp"`
+	TimeStamp string `json:"timestamp"`
 }
