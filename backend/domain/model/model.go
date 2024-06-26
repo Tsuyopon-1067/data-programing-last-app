af5af7dd37d702b58bef864e10011c7dc0e28c19
Shibatch <lawnbone.tubist@gmail.com>
2024-06-26 10:21:50 +0900

    ✏️ TymeStamp -> TimeStamp

--- backend/domain/model/model.go
+++ backend/domain/model/model.go
@@ -5,11 +5,11 @@ import "time"
 type Message struct {
 	Username string `json:"username"`
 	Message  string `json:"message"`
-	TymeStamp time.Time `json:"timestamp"`
+	TimeStamp time.Time `json:"timestamp"`
 }
 
 type Response struct {
 	Username string `json:"username"`
 	Message  string `json:"message"`
-	TymeStamp time.Time `json:"timestamp"`
+	TimeStamp time.Time `json:"timestamp"`
 }
