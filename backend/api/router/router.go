761ded088deb9f5056957e1dd5b2562040101c33
Shibatch <lawnbone.tubist@gmail.com>
2024-06-26 16:57:41 +0900

    ✨ CSVをダウンロードするためのルーティングを設定

--- backend/api/router/router.go
+++ backend/api/router/router.go
@@ -8,4 +8,6 @@ import (
 
 func SetupRoutes(e *echo.Echo) {
 	e.GET("/ws", handler.WebSocketHandler)
+	e.GET("/username", handler.HandleUsernameSortedCSV)
+	e.GET("/date", handler.HandleDateSortedCSV)
 }
