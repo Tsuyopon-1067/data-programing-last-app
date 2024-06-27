c3468c45e305ff99816ef565644130c700227260
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-28 05:51:43 +0900

    🐛️ 競合を解消

--- frontend/src/components/Templates/Xframe.tsx
+++ frontend/src/components/Templates/Xframe.tsx
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
 import { LinkList } from "../organisms/LinkList";
 import { NaviMenu } from "../organisms/NavMenu";
-=======
-import { NaviMenue } from "../organisms/NavMenue";
-import { NewsList } from "../organisms/NewsList";
->>>>>>> Stashed changes
 import { TimeLine } from "../organisms/TimeLine";
 import styles from "./Xframe.module.css";
 
@@ -12,22 +7,13 @@ export const Xframe = () => {
   return (
     <div className={styles.main}>
       <div className={styles.nav}>
-<<<<<<< Updated upstream
         <NaviMenu />
-=======
-        <NaviMenue />
->>>>>>> Stashed changes
       </div>
       <div className={styles.timeline}>
         <TimeLine />
       </div>
-<<<<<<< Updated upstream
       <div className={styles.links}>
         <LinkList />
-=======
-      <div className={styles.others}>
-        <NewsList />
->>>>>>> Stashed changes
       </div>
     </div>
   );
