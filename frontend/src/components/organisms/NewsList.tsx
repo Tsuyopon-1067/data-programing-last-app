f503be385d8d645dc1e285d8e39567b4841d58c1
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-28 05:46:52 +0900

    💄 タイトルの追加

--- frontend/src/components/organisms/NewsList.tsx
+++ frontend/src/components/organisms/NewsList.tsx
@@ -5,7 +5,10 @@ import NewsItem from "../molecules/NewsItem";
 import styles from "./NewsList.module.css";
 
 export const NewsList = () => {
-  const[newsList, setNewsList] = useState<News[]>([]);
+  //const[newsList, setNewsList] = useState<News[]>([]);
+  const[newsList, setNewsList] = useState<News[]>(
+    JSON.parse(`[{"title":"28日は大雨 九州は線状降水帯恐れ","category":"国内","url":"https://news.yahoo.co.jp/pickup/6505754","comments":73},{"title":"中国 歴代国防相2人の党籍を剥奪","category":"国際","url":"https://news.yahoo.co.jp/pickup/6505780","comments":390},{"title":"37年半ぶりの円安水準 高まる緊張","category":"経済","url":"https://news.yahoo.co.jp/pickup/6505771","comments":1692},{"title":"SnowManラウール 仏でモデル所属","category":"エンタメ","url":"https://news.yahoo.co.jp/pickup/6505790","comments":271}]`)
+  );
   useEffect(() => {
     fetch('http://localhost:8080/fetch/news')
       .then(response => {
@@ -43,6 +46,9 @@ export const NewsList = () => {
         borderRadius: "20px",
       }}
     >
+      <div className={styles.topArea}>
+        <span className={styles.topText}>いまどうなってる？</span>
+      </div>
       <NewsItems />
       <a className={styles.a} href="https://news.yahoo.co.jp/">
       <div className={styles.bottomArea}>
