2f389642346bb35fd79ed8fde4aa7f3eda81ff02
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-28 03:33:35 +0900

    💄 navigationのボタンを追加

--- frontend/src/components/organisms/NavMenu.tsx
+++ frontend/src/components/organisms/NavMenu.tsx
@@ -1,11 +1,24 @@
+import { Home } from "@mui/icons-material";
 import { Stack } from "@mui/material";
+import { NaviButton } from "../molecules/NaviButton";
 
 export const NaviMenu = () => {
   return (
-    <Stack direction={"column"} spacing={2}>
-      hogehoge
-      hogehoge
-      hogehoge
+    <Stack
+      direction={"column"}
+      spacing={2}
+      marginRight={3}
+      height={"100vh"}
+      sx={{
+        borderRight: "1px solid",
+        borderColor: "#EFF3F4",
+      }}
+    >
+      <NaviButton name="ホーム"><Home sx={{fontSize: "28px"}} /></NaviButton>
+      <NaviButton name="ホーム"><Home sx={{fontSize: "28px"}} /></NaviButton>
+      <NaviButton name="ホーム"><Home sx={{fontSize: "28px"}} /></NaviButton>
+      <NaviButton name="ホーム"><Home sx={{fontSize: "28px"}} /></NaviButton>
+      <NaviButton name="ホーム"><Home sx={{fontSize: "28px"}} /></NaviButton>
     </Stack>
   );
 }
