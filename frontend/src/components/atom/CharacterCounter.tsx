b3a3ff056a0528b575b0d6007158515113465866
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-07-02 21:27:29 +0900

    💄  130字を超えたとき文字数カウンタに文字数を表示

--- frontend/src/components/atom/CharacterCounter.tsx
+++ frontend/src/components/atom/CharacterCounter.tsx
@@ -1,14 +1,44 @@
-import { CircularProgress } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 
 export const CharacterCounter: React.FC<{ value: number }> = ({ value }) => {
-  const circleValue = (value / 140) * 100;
+  let circleValue = (value / 140) * 100;
+  let color: "primary" | "warning" | "error" = "primary";
+  let labelValue = "";
+  if (value > 130) {
+    labelValue = (140 - value).toString();
+  }
+
+  if (140 < value) {
+    color = "error";
+    circleValue = 100;
+  } else if (130 < value) {
+    color = "warning";
+  }
   return (
     <div>
       <CircularProgress
         variant="determinate"
         size={"100%"}
         value={circleValue}
+        color={color}
       />
+      <Box
+        sx={{
+          top: -25,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'relative',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          variant="caption"
+          component="div"
+          color="text.secondary"
+        >{labelValue}</Typography>
+      </Box>
     </div>
   );
 };
