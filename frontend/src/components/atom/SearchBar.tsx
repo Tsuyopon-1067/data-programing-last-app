f7f20289291bf1fde826ec663ba5bc7a45f4c91e
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-07-03 00:35:25 +0900

    💄 検索バーの色を変更

--- frontend/src/components/atom/SearchBar.tsx
+++ frontend/src/components/atom/SearchBar.tsx
@@ -18,9 +18,33 @@ export const SearchBar = () => {
       }}
       variant="outlined"
       sx={{
+        borderStyle: 'none',
         '& .MuiOutlinedInput-root': {
           borderRadius: '20px',
           height: '40px',
+          transition: 'border-color 0.3s',
+          backgroundColor: '#eff3f4',
+          borderColor: '#eff3f4',
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'initial',
+          },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#1ea1f1',
+          },
+          '&.Mui-focused': {
+            backgroundColor: '#ffffff',
+            borderColor: '#1ea1f1',
+            '& .MuiSvgIcon-root': {
+              color: '#1ea1f1',
+            },
+          },
+        },
+        '& .MuiInputLabel-root': {
+          transition: 'color 0.3s',
+          top: '-6px',
+          '&.Mui-focused': {
+            color: '#1ea1f1',
+          },
         },
       }}
     />
