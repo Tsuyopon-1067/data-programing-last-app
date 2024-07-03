fb95f48f000e40f8d3fb3c0ba6968b7bb3160565
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-07-03 16:43:52 +0900

    💄  1150px以下のレスポンシブ対応を実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,43 @@ import { Xframe } from "./components/templetes/Xframe";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TechStack } from "./components/pages/TechStack";
 import { Qr } from "./components/pages/Qr";
+import { createContext, useEffect, useState } from "react";
+import { Responsive } from "./types/responsivie";
+import { useWindowWidth } from "./hooks/UseWindowWidth";
+
+interface ResponsiveContextType {
+  value: Responsive;
+  setValue: React.Dispatch<React.SetStateAction<Responsive>>;
+}
+export const ResponsiveContext = createContext<ResponsiveContextType | undefined>(undefined);
 
 function App() {
+  const [responsive, setResponsive] = useState<Responsive>("pc");
+  const width = useWindowWidth();
+  useEffect(() => {
+    if (width < 600) {
+      setResponsive("mobile");
+    } else if (width < 1150) {
+      setResponsive("tablet");
+    } else {
+      setResponsive("pc");
+    }
+  }, [width]);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Xframe />}>
-            <Route path="/" element={<TimeLineLinkList />} />
-            <Route path="/slide" element={<Slide />} />
-            <Route path="/techStack" element={<TechStack />} />
-            <Route path="/architecture" element={<Architecture />} />
-            <Route path="/qr" element={<Qr />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <ResponsiveContext.Provider value={{ value: responsive, setValue: setResponsive }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Xframe />}>
+              <Route path="/" element={<TimeLineLinkList />} />
+              <Route path="/slide" element={<Slide />} />
+              <Route path="/techStack" element={<TechStack />} />
+              <Route path="/architecture" element={<Architecture />} />
+              <Route path="/qr" element={<Qr />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ResponsiveContext.Provider>
     </>
   );
 }
--- frontend/src/components/molecules/NaviButton.tsx
+++ frontend/src/components/molecules/NaviButton.tsx
@@ -1,10 +1,39 @@
-import { Button } from "@mui/material";
-import { ReactNode } from "react";
+import { Button, IconButton } from "@mui/material";
+import { ReactNode, useContext } from "react";
 import styles from "./NaviButton.module.css";
 import { useNavigate } from "react-router-dom";
+import { ResponsiveContext } from "../../App";
 
-export const NaviButton: React.FC<{ children: ReactNode, name: string, url: string }> = ({ children, name, url }) => {
+export const NaviButton: React.FC<{
+  children: ReactNode;
+  name: string;
+  url: string;
+}> = ({ children, name, url }) => {
   const navigate = useNavigate();
+  const responsive = useContext(ResponsiveContext);
+  if (responsive?.value === "pc") {
+    return (
+      <Button
+        onClick={() => navigate(url)}
+        variant="text"
+        sx={{
+          color: "black",
+          fontSize: "22px",
+          borderRadius: "16px",
+        }}
+      >
+        <div className={styles.buttonGrid}>
+          <div className={styles.iconArea}>
+            <div className={styles.iconMiddle}> {children} </div>
+          </div>
+          <div className={styles.nameArea}>{name}</div>
+        </div>
+      </Button>
+    );
+  }
+  if (responsive?.value === "tablet") {
+    return <IconButton onClick={() => navigate(url)}>{children}</IconButton>;
+  }
   return (
     <Button
       onClick={() => navigate(url)}
@@ -19,10 +48,8 @@ export const NaviButton: React.FC<{ children: ReactNode, name: string, url: stri
         <div className={styles.iconArea}>
           <div className={styles.iconMiddle}> {children} </div>
         </div>
-        <div className={styles.nameArea}>
-          {name}
-        </div>
+        <div className={styles.nameArea}>{name}</div>
       </div>
     </Button>
   );
-}
+};
