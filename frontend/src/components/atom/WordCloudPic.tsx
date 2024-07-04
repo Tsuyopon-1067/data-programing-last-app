6aced68a6f3a27625c4e8bc690c1c3a0e9bba06a
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-07-05 00:32:23 +0900

    💄 word cloud表示領域の大きさ調整

--- frontend/src/components/atom/WordCloudPic.tsx
+++ frontend/src/components/atom/WordCloudPic.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './WordCloudPic.module.css';
 
 export const WordCloudPic: React.FC<{ finishLoading: () => void }> = ({ finishLoading }) => {
   return (
-    <img
+    <img className={styles.img}
       src="./fetch/wordcloud"
       alt="word cloud image"
       onLoad={() => finishLoading()}
