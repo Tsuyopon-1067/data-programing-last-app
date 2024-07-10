4aa3c1591087e0d7d128475ee6a5382f29a7ef99
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-07-10 13:20:07 +0900

    💄 ニュースを項目にダークテーマを実装

--- frontend/src/components/molecules/NewsItem.tsx
+++ frontend/src/components/molecules/NewsItem.tsx
@@ -1,15 +1,29 @@
+import { useDarkTheme } from "../templetes/DarkThemeProvider";
 import styles from "./NewsItem.module.css";
 
-export const NewsItem: React.FC<{ title: string, url: string, category: string, comment: number }> = ({title, url, category, comment}) => {
+export const NewsItem: React.FC<{
+  title: string;
+  url: string;
+  category: string;
+  comment: number;
+}> = ({ title, url, category, comment }) => {
+  const { fontColor, isDarkMode } = useDarkTheme();
+  const subFontColor = isDarkMode ? "#71767B" : "#536471";
   return (
     <a className={styles.a} href={url}>
       <div className={styles.main}>
-        <p className={styles.category}>{category}</p>
-        <h2 className={styles.title}>{title}</h2>
-        <p className={styles.comment}>{comment} comments</p>
+        <p className={styles.category} style={{ color: subFontColor }}>
+          {category}
+        </p>
+        <h2 className={styles.title} style={{ color: fontColor }}>
+          {title}
+        </h2>
+        <p className={styles.comment} style={{ color: subFontColor }}>
+          {comment} comments
+        </p>
       </div>
     </a>
   );
-}
+};
 
 export default NewsItem;
